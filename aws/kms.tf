1505dfb7df8b43eebfaa330901ab8a8cc070c681
MYINT <luvmyo@gmail.com>
2022-02-08 16:41:36 +0700

    kms-added

--- aws/kms.tf
+++ aws/kms.tf
@@ -1,7 +1,6 @@
 resource "aws_kms_key" "logs_key" {
   # key does not have rotation enabled
   description = "${local.resource_prefix.value}-logs bucket key"
-
   deletion_window_in_days = 7
 
 resource "aws_kms_alias" "logs_key_alias" {
